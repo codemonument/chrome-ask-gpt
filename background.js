a40f6611155f9f35ee5e0617478783e3827cd533
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-01 12:42:10 +0100

    add basic event handler for omnibar handling

--- background.js
+++ background.js
@@ -20,3 +20,28 @@ chrome.runtime.onInstalled.addListener(details => {
 	// Debug output for easier development
 	console.log(`Ask GPT Extension changed because of: ${details.reason}!`);
 });
+
+/**
+ * Called once per input session and before any onInputChanged Events
+ */
+chrome.omnibox.onInputStarted.addListener(() => {
+	console.log(`Omnibox input started`);
+});
+
+/**
+ * User has changed what is typed into the omnibox.
+ */
+chrome.omnibox.onInputChanged.addListener((text, suggestCallback) => {
+	console.log(`Omnibox input changed`, text);
+});
+
+chrome.omnibox.onInputEntered.addListener((text, dispositionCallback) => {
+	console.log(`Omnibox input entered`, text);
+});
+
+/**
+ * User has ended the keyword input session without accepting the input.
+ */
+chrome.omnibox.onInputCancelled.addListener(() => {
+	console.log(`Omnibox input cancelled`);
+});
