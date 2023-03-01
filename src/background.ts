5d8786661e876617b93dbdc6ddf7fdc01dc86e83
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-01 17:17:03 +0100

    add short-circuit login

--- src/background.ts
+++ src/background.ts
@@ -1,4 +1,5 @@
 import { userLoggedIn } from './lib/state.js';
+import { z } from "zod";
 import './manifest.json';
 
 /**
@@ -42,10 +43,10 @@ chrome.omnibox.onInputStarted.addListener(async () => {
 	console.debug(`Fetch result!`, res);
 	// Note: chat.openai.com returns a 403 when fetched without login cretendials, instead of redirecting to the login page.
 	if (res.status === 403) {
-		console.debug(`User is not logged in! => Suggesting login at https://chat.openai.com`);
+		console.debug(`User is not logged in! => Suggesting login at https://chat.openai.com/auth/login`);
 		chrome.omnibox.setDefaultSuggestion({
 			description:
-				'<dim>You seem to not be logged in! Please log in at <url>https://chat.openai.com</url></dim>!',
+				'You seem to not be logged in! Please log in at <url>https://chat.openai.com/auth/login</url>!',
 			// content: 'https://chat.openai.com/auth/login',
 			// deletable: false,
 		});
@@ -64,27 +65,51 @@ chrome.omnibox.onInputChanged.addListener((text, suggestCallback) => {
 	console.log(`Omnibox input changed`, text);
 });
 
-chrome.omnibox.onInputEntered.addListener(async (text, dispositionCallback) => {
-	console.log(`Omnibox input entered`, text);
-	// navigateToChatGPT();
-	const tab = await chrome.tabs.create({
+chrome.omnibox.onInputEntered.addListener(async (text, disposition) => {
+	console.debug(`Omnibox input entered`, { text, disposition, userLoggedIn: userLoggedIn.value });
+
+	// Short-Circuit to chatGPT login
+	if (userLoggedIn.value === false) {
+		console.debug(`Use shortcut redirect to chatGPT login!`);
+		chrome.tabs.update({
+			url: 'https://chat.openai.com/auth/login'
+		})
+		return;
+	}
+
+	const navigationObj: chrome.tabs.UpdateProperties = {
 		url: 'https://chat.openai.com/chat',
-	});
+	}
+
+	let tabPromise;
+	switch (disposition) {
+		case 'currentTab':
+			// tabId will default to current tab (which is enough for me bc. this extension is an omnibox one)
+			tabPromise = chrome.tabs.update(navigationObj)
+			break;
+		case 'newForegroundTab':
+			tabPromise = chrome.tabs.create(navigationObj);
+			break;
+		case 'newBackgroundTab':
+			navigationObj.active = false;
+			tabPromise = chrome.tabs.create(navigationObj);
+			break;
+	}
+
+	// Navigate To ChatGPT
+	const tab = await tabPromise;
+	const tabId = z.number().parse(tab.id);
 
 	chrome.scripting.executeScript({
-		target: { tabId: tab.id },
-		function: () => {
+		target: { tabId },
+		func() {
 			const promptTextArea = document.querySelector('textarea[data-id=root]');
+			const confirmButton = document.querySelector('textarea[data-id=root] + button')
 
-			// const confirmButton = document.querySelector('textarea[data-id=root] + button')
-			const element = document.getElementById('my-element');
-			if (element) {
-				console.log(element.textContent);
-			}
+			console.debug(`Maybe found Elements: `, { promptTextArea, confirmButton })
 		},
 	});
 
-	// askChatGPT(text);
 });
 
 /**
