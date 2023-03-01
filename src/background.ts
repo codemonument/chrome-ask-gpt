c2122fe8101f81194dd69b7d4fbfd0316ee10e7c
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-02 00:52:37 +0100

    remove unused code comments

--- src/background.ts
+++ src/background.ts
@@ -7,20 +7,6 @@ import { insertTextIntoChatGPT } from './lib/client-scripts';
  * Main Extension Service Worker as ESModule
  */
 
-// chrome.action.onClicked.addListener((tab) => {
-//     chrome.scripting.executeScript({
-//         target: {tabId: tab.id},
-//         function: () => {
-//             const query = new URLSearchParams(tab.url).get('q');
-//             const inputField = document.querySelector('[placeholder="Ask me anything..."]');
-//             if (inputField) {
-//                 inputField.value = query;
-//                 inputField.form.submit();
-//             }
-//         }
-//     });
-//  });
-
 chrome.runtime.onInstalled.addListener(details => {
 	// Debug output for easier development
 	console.log(`Ask GPT Extension changed because of: ${details.reason}!`);
