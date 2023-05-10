f1147f648b2025d4d53ff0f38c41401f9d06ccb6
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-05-10 23:08:32 +0200

    make popup render via solid-js

---
+++ src/pages/popup/Popup.tsx
@@ -0,0 +1,26 @@
+import logo from "@assets/ChatGPT_logo.svg";
+import "@src/styles/index.css";
+import styles from "./Popup.module.css";
+
+const Popup = () => {
+	return (
+		<div class={styles.App}>
+			<header class={styles.header}>
+				<img src={logo} class={styles.logo} alt="logo" />
+				<p class="font-bold">
+					Edit <code>src/pages/popup/Popup.tsx</code> and save to reload.
+				</p>
+				<a
+					class={styles.link}
+					href="https://github.com/solidjs/solid"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn Solid
+				</a>
+			</header>
+		</div>
+	);
+};
+
+export default Popup;
